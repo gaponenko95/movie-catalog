ee3e7f13f2d9a0ab6ee209da476148abfb9bf18c
Aleksander Gaponenko <sashagaponenko01904@yandex.ru>
2024-03-16 22:27:36 +0300

    fix MainForm

--- src/components/MainForm/MainForm.jsx
+++ src/components/MainForm/MainForm.jsx
@@ -2,26 +2,17 @@ import './MainForm.css';
 import Button from '../Button/Button.jsx';
 
 function MainForm({ placeholder, icon, buttonText, column, onSubmit }) {
-	const inputClass = icon === 'search' ? 'main-form__input icon' : 'main-form__input';
-
-	const handleSubmit = e => {
-		e.preventDefault();
-		const value = new FormData(e.target).get('search');
-	};
+	const formClass = `form ${column ? 'form--column' : ''}`;
+	const inputClass = `form__input ${icon === 'search' ? 'form__input--icon' : ''}`;
 
 	return (
-		<form className={`main-form ${column ? 'column' : ''}`} onSubmit={onSubmit}>
-			<div className='main-form__input-wrapper'>
-				<input
-					type='search'
-					name='search'
-					placeholder={placeholder}
-					className={inputClass}
-				/>
-				{icon === 'search' && (
-					<img className='main-form__icon' src='/search.svg' alt='Search' />
-				)}
-			</div>
+		<form className={formClass} onSubmit={onSubmit}>
+			<input
+				className={inputClass}
+				type='search'
+				name='search'
+				placeholder={placeholder}
+			/>
 			<Button text={buttonText} />
 		</form>
 	);
