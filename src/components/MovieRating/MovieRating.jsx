5569687a52f522fb9850462e73031e3c40d6349e
Aleksander Gaponenko <sashagaponenko01904@yandex.ru>
2024-03-17 17:25:29 +0300

    add module.css for all components and layouts

--- src/components/MovieRating/MovieRating.jsx
+++ src/components/MovieRating/MovieRating.jsx
@@ -3,7 +3,7 @@ import styles from './MovieRating.module.css';
 function MovieRating({ rating }) {
 	return (
 		<div className={styles.movieItemRating}>
-			<img src='/star.svg' />
+			<img src='/star.svg' alt='Рейтинг' />
 			{rating}
 		</div>
 	);
